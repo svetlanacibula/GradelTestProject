383fc66cdb6883fac5774ca35a088b676d05159a
svetlanacibula <gaisma.sipola@gmail.com>
2020-11-08 17:58:26 +0200

    Forum Cinema and Janis Roze homework

---
+++ src/main/java/Lesson11/Common.java
@@ -0,0 +1,23 @@
+package Lesson11;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Common {
+    public static WebDriver driver;
+
+    public void startPage(String url) {
+        String driverPath = "C:\\Users\\Waltion\\IdeaProjects\\Gradel TestProject\\src\\test\\resources\\";
+        System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+
+    }
+    public void StopDriver() {
+        driver.quit();
+    }
+
+
+}
+
